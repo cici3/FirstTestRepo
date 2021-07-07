b5e705aad6acdedc2d4f5ba046dbc3a0a62ce280
cici3 <86908667+cici3@users.noreply.github.com>
2021-07-07 17:34:29 +0530

    Update README.cmd
    Update to readme

--- README.cmd
+++ README.cmd
@@ -4,4 +4,4 @@ This is an example repository for the Git tutoial on https://www.w3schools.com
 
 This repository is built step by step in the tutorial.
 
-Adding this line to test pulling from Github
+Adding this line to test pulling from Github - test
